6918cfba7f5fc0bd74ca9f31427b62dbf836d0ce
Xnco <xuanyylhky@163.com>
2018-02-26 18:00:53 +0800

    Add Staff Info
    添加员工和职业的信息

---
+++ Assets/Resources/Scripts/Staff/Job.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum JobType
+{
+    Coder = 1, // 程序员
+    Writer,        // 脚本策划
+    Designer,   // 图像师
+    Snd_Eng,  // 音效师
+    Director,    // 总监
+    Producer,  // 制片人
+    Hardware, // 硬件工程师
+    Hacker,     // 黑客
+}
+
+public class Job
+{
+    public JobType mType;
+    public int mLevel;     // 等级
+    public float mCurExp;    // 当前职业经验
+    public JobLevelUpInfo mInfo; // 职业信息
+}
+
+public struct JobInfo
+{
+    // 职业每级对应的升级的信息
+    Dictionary<int, JobLevelUpInfo> mAllLevelInfo;
+}
+
+public struct JobLevelUpInfo
+{
+    // 下一级， 如果为0说明当前为最高级
+    public int mNextLevel;
+    // 升级需要经验
+    public float mNeedExp;
+
+    public float mProgram;     // 编程加成
+    public float mScenario;    // 脚本加成
+    public float mGraphics;    // 图像加成
+    public float mSound;        // 声音加成
+}
---
+++ Assets/Resources/Scripts/Staff/Staff.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff
+{
+    private string mName;     // 名字
+    private Job mCurJob;     // 职位
+    private List<Job> mJobs;    // 职业生涯
+    private float mSalary;      // 薪水
+    private float mPower;      // 体力
+
+    // 四维
+    private float mProgram;     // 编程
+    private float mMaxProgram;
+    private float mScenario;    // 脚本
+    private float mMaxScenario;
+    private float mGraphics;    // 图像
+    private float mMaxGraphics; 
+    private float mSound;        // 声音
+    private float mMaxSound;
+
+    // 隐性属性
+    private float mTalent;      // 天赋, 隐藏思维加成
+    private float mDiligent;    // 勤奋程度, 影响工作频率
+    private float mEffectiveness;   // 效率, 影响工作速度
+    private int mStrata;    // 阶层, 影响招人渠道
+
+    public float pProgram
+    {
+        get
+        {
+            // 显示能力为 人物能力 + 职业加成能力
+            return mProgram + mCurJob.mInfo.mProgram;
+        }
+    }
+}
